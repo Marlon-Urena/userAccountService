ede6f49227adbe63169e266908eb071176955487
Marlon-Urena <anewryurena@gmail.com>
2022-01-30 19:42:31 +0000

    Removed creationTimestamp and updateTimestamp attributes.

--- src/main/java/com/chatapp/model/UserAccount.java
+++ src/main/java/com/chatapp/model/UserAccount.java
@@ -1,11 +1,7 @@
 package com.chatapp.model;
 
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import javax.persistence.*;
-import java.time.OffsetDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -28,12 +24,4 @@ public class UserAccount {
     private String state;
     private String country;
     private String zipCode;
-
-    @CreationTimestamp
-    @Column(name = "created_at", updatable = false)
-    private OffsetDateTime createdAt;
-
-    @UpdateTimestamp
-    @Column(name = "updated_at")
-    private OffsetDateTime updatedAt;
 }
