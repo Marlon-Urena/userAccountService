ebeb5e59e1c7a84ef5a7feffbf6f9b4450fc8622
Marlon <marlon.urena@cix.csi.cuny.edu>
2022-07-02 09:32:50 -0400

    Removed unused assembler

--- src/main/java/com/chatapp/assembler/UserAccountResourceAssembler.java
+++
@@ -1,9 +0,0 @@
-package com.chatapp.assembler;
-
-import com.chatapp.controller.UserAccountController;
-import com.chatapp.model.UserAccount;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.RepresentationModelAssembler;
-import org.springframework.stereotype.Component;
-
-
